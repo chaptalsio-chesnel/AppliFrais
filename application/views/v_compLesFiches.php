acc15e4e25d5a96f26e4bc6ec137964206a99c14
cadoret <cadoret@B4-W7-14.sio.chaptalqper>
2016-11-29 13:23:59 +0100

    Indentation

--- application/views/v_compLesFiches.php
+++ application/views/v_compLesFiches.php
@@ -1,5 +1,5 @@
 <?php
-	$this->load->helper('url');
+$this->load->helper ( 'url' );
 ?>
 <div id="contenu">
 	<h2>Liste des fiches de frais</h2>
@@ -9,40 +9,36 @@
 	<table class="listeLegere">
 		<thead>
 			<tr>
-				<th >Mois</th>
-				<th >Utilisateur</th>
-				<th >Montant</th>  
-				<th >Date modif.</th>  
-				<th  colspan="4">Actions</th>              
+				<th>Mois</th>
+				<th>Utilisateur</th>
+				<th>Montant</th>
+				<th>Date modif.</th>
+				<th colspan="4">Actions</th>
 			</tr>
 		</thead>
 		<tbody>
           
-		<?php    
-			foreach( $lesFiches as $uneFiche) 
-			{
-				$modLink = '';
-				$signeLink = '';
-
-				if ($uneFiche['id'] == 'CL') {
-					$signeLink = anchor('c_comptable/validerFiche/'.$uneFiche['mois'].'/'.$uneFiche['idv'], 'valider',  'title="Signer la fiche"  onclick="return confirm(\'Voulez-vous vraiment valider cette fiche ?\');"');
-					$refusLink = anchor('c_comptable/refusFiche/'.$uneFiche['mois'].'/'.$uneFiche['idv'], 'refuser',  'title="Refiser la fiche"  onclick="return confirm(\'Voulez-vous vraiment refuser cette fiche ?\');"');
-					echo
-					'<tr>
-					<td class="date">'.anchor('c_comptable/voirFiche/'.$uneFiche['mois'].'/'.$uneFiche['idv'], $uneFiche['mois'],  'title="Consulter la fiche"').'</td>
-					<td class="libelle">'.$uneFiche['nom'].'</td>
-					<td class="montant">'.$uneFiche['montantValide'].'</td>
-					<td class="date">'.$uneFiche['dateModif'].'</td>
-					<td class="action">'.$signeLink.'</td>
-					<td class="action">'.$refusLink.'</td>
-					<td class="action">'.anchor('c_comptable/modCompFiche/'.$uneFiche['mois'].'/'.$uneFiche['idv'], "Modifier",  'title="Modifier la fiche"').'</td>
+		<?php
+		foreach ( $lesFiches as $uneFiche ) {
+			$modLink = '';
+			$signeLink = '';
+			
+			if ($uneFiche ['id'] == 'CL') {
+				$signeLink = anchor ( 'c_comptable/validerFiche/' . $uneFiche ['mois'] . '/' . $uneFiche ['idv'], 'valider', 'title="Signer la fiche"  onclick="return confirm(\'Voulez-vous vraiment valider cette fiche ?\');"' );
+				$refusLink = anchor ( 'c_comptable/refusFiche/' . $uneFiche ['mois'] . '/' . $uneFiche ['idv'], 'refuser', 'title="Refiser la fiche"  onclick="return confirm(\'Voulez-vous vraiment refuser cette fiche ?\');"' );
+				echo '<tr>
+					<td class="date">' . anchor ( 'c_comptable/voirFiche/' . $uneFiche ['mois'] . '/' . $uneFiche ['idv'], $uneFiche ['mois'], 'title="Consulter la fiche"' ) . '</td>
+					<td class="libelle">' . $uneFiche ['nom'] . '</td>
+					<td class="montant">' . $uneFiche ['montantValide'] . '</td>
+					<td class="date">' . $uneFiche ['dateModif'] . '</td>
+					<td class="action">' . $signeLink . '</td>
+					<td class="action">' . $refusLink . '</td>
+					<td class="action">' . anchor ( 'c_comptable/modCompFiche/' . $uneFiche ['mois'] . '/' . $uneFiche ['idv'], "Modifier", 'title="Modifier la fiche"' ) . '</td>
 				</tr>';
-				}
-				
-
 			}
+		}
 		?>	  
 		</tbody>
-    </table>
+	</table>
 
 </div>
